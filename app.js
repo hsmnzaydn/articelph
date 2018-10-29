42db021b0ba7c311e7a29181adbffe6cc92e9dcd
Serkan Ozaydin <serkan.zaydn@gmail.com>
2018-10-29 13:05:40 +0300

    Changed start point of server

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express=require('express');
       app=express();
       swaggerUI=require('swagger-ui-express');
       YAML = require('yamljs');
-      port= 80 || process.env.PORT
+      port= 8080 || process.env.PORT
       
 //Initialize
     swaggerDocument = YAML.load('./api/swagger_api.yaml');      
